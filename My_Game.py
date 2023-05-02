69b73ed52d76843591e2f5ae862925681bd96814
PalinV <palinv@mail.ru>
2023-05-02 09:28:04 +0300

    funk out class

--- My_Game.py
+++ My_Game.py
@@ -1,3 +1,4 @@
+import random
 class Human:
     width = 5
     length = 5
@@ -6,212 +7,194 @@ class Human:
         self.coord_x = 0
         self.coord_y = 0
         self.vectors = []
-    
-    def possible_vector_go(self):
-        if self.coord_y != 1 and self.coord_y != self.length and self.coord_x != 1 and self.coord_x != self.width:
-           self.vectors = ['left', 'front', 'back', 'right'] 
-        if self.vector_face == 1:
-        # Перемещение по крайним верхней и нижней линиям  оси Х <->
-            if self.coord_y == 1 and 1 < self.coord_x < self.length :
-                self.vectors = ['left', 'front', 'back']
-            elif self.coord_y == self.width and 1 < self.coord_x < self.length:
-                self.vectors = ['right', 'front', 'back']
-            elif self.coord_y == 1 and self.coord_x == 1:
-                self.vectors = ['left', 'front']
-            elif self.coord_y == 1 and self.coord_x == self.length:
-                self.vectors = ['left', 'back']
-            elif self.coord_y == self.width and self.coord_x == 1:
-                self.vectors = ['right', 'front']
-            elif self.coord_y == self.width and self.coord_x == self.length:
-                self.vectors = ['right', 'back']
-        # Уперся в правый край граница ось У ->
-            elif self.coord_x == self.length and (self.coord_y != self.length and self.coord_y != 1 ):
-                self.vectors = ['left', 'right', 'back']
-        
-        elif self.vector_face == 2:
-            if self.coord_x == 1 and 1 < self.coord_y < self.length :
-                self.vectors = ['right', 'front', 'back']
-            elif self.coord_x == self.width and 1 < self.coord_y < self.length:
-                self.vectors = ['left', 'front', 'back']
-            elif self.coord_y == 1 and self.coord_x == 1:
-                self.vectors = ['right', 'front']
-            elif self.coord_y == 1 and self.coord_x == self.length:
-                self.vectors = ['right', 'front']
-            elif self.coord_y == self.width and self.coord_x == 1:
-                self.vectors = ['right', 'back'] 
-            elif self.coord_y == self.width and self.coord_x == self.length:
-                self.vectors = ['left', 'back']
-            # Уперся в верхний край граница ось У ->
-            elif self.coord_y == self.length and (self.coord_x != self.length and self.coord_x != 1 ):
-                self.vectors = ['left', 'right', 'back']
-        
-        elif self.vector_face == -1:
-            if self.coord_y == 1 and 1 < self.coord_x < self.length :
-                self.vectors = ['right', 'front', 'back']
-            elif self.coord_y == self.width and 1 < self.coord_x < self.length:
-                self.vectors = ['left', 'front', 'back']
-            elif self.coord_y == 1 and self.coord_x == 1:  
-                self.vectors = ['right', 'back']
-            elif self.coord_y == 1 and self.coord_x == self.length:
-                self.vectors = ['right', 'front']
-            elif self.coord_y == self.width and self.coord_x == 1:
-                self.vectors = ['left', 'back']
-            elif self.coord_y == self.width and self.coord_x == self.length:
-                self.vectors = ['left', 'front']
-            # Уперся в левый край граница ось У ->
-            elif self.coord_x == 1 and (self.coord_y != self.length and self.coord_y != 1 ):
-                self.vectors = ['left', 'right', 'back']
-        
-        elif self.vector_face == -2:
-            if self.coord_x == 1 and 1 < self.coord_y < self.length :
-                self.vectors = ['left', 'front', 'back']
-            elif self.coord_x == self.width and 1 < self.coord_y < self.length:
-                self.vectors = ['right', 'front', 'back']
-            elif self.coord_y == 1 and self.coord_x == 1:
-                self.vectors = ['left', 'back']
-            elif self.coord_y == 1 and self.coord_x == self.length:
-                self.vectors = ['right', 'back']
-            elif self.coord_y == self.width and self.coord_x == 1:
-                self.vectors = ['left', 'front']
-            elif self.coord_y == self.width and self.coord_x == self.length:
-                self.vectors = ['right', 'back']
-            # Уперся в нижний край граница ось У ->
-            elif self.coord_y == 1 and (self.coord_x != self.length and self.coord_x != 1 ):
-                self.vectors = ['left', 'right', 'back']
-
 hero = Human()
 hero.vector_face = 1
 hero.coord_x = 1
 hero.coord_y = 1
 hero.vectors = []
-def move_hero(vertor_go):
-    for i in (1, -1, 2, -2):
-        if i < 0:
-            change_coord = -1
-        else:
-            change_coord = 1
-        if i == hero.vector_face:
-            for j in range(2, 10, 2):
-                if str(j) == vertor_go:
-                    if j == 2:
-                        hero.vector_face *= -1
-                    # Было Х меняется на У
-                    elif j == 4 and hero.vector_face % 2: # 1 или -1
-                        if hero.vector_face < 0:
-                            hero.vector_face -=  1 
-                        else:
-                            hero.vector_face += 1
-                    # Было У меняется на Х
-                    elif j == 4 and not (hero.vector_face % 2):     # 2 или -2
-                        hero.vector_face = int(hero.vector_face / 2) * -1 if not (hero.vector_face % 2) else (hero.vector_face + change_coord) * -1
-                     # Было Х меняется на У
-                    elif j == 6 and hero.vector_face % 2:
-                        if hero.vector_face < 0: 
-                            vector_face = (hero.vector_face - 1) * -1 
-                        else:
-                            vector_face = (hero.vector_face + 1) * -1
-                    # Условие У
-                    elif j == 6 and not (hero.vector_face % 2):
-                        hero.vector_face = int(hero.vector_face / 2) if not (hero.vector_face $ 2) else hero.vector_face + change_coord
-                    
-                    if hero.vector_face % 2:   # Для Х
-                        hero.coord_x += change_coord
-                    else:                           # Для У
-                        hero.coord_y += change_coord                
-            break            
-    
-    
-    
-    
-    # if hero.vector_face == 'x':
-    #     if vertor_go == "8":
-    #         hero.vector_face = "x"
-    #         hero.coord_x += 1
-    #     elif vertor_go == "2":
-    #         hero.coord_x -= 1
-    #         hero.vector_face = "-x"
-    #     elif vertor_go == "4":           
-    #         hero.vector_face = "y"
-    #         hero.coord_y += 1
-    #     elif vertor_go == "6":
-    #         hero.vector_face = "-y"
-    #         hero.coord_y -= 1
+bot = Human()
+bot.vector_face = -1
+bot.coord_x = bot.width
+bot.coord_y = bot.length
+
+# Объявляется функция определяющая возможные направления движения с учетом статического направления
+def possible_vector_go(vector_face, coord_y, coord_x):
+    if coord_y != 1 and coord_y != Human.length and coord_x != 1 and coord_x != Human.width:
+       vectors = ['left', 'front', 'back', 'right'] 
+    if vector_face == 1:
+        # Перемещение по крайним верхней и нижней линиям  оси Х <->
+        if coord_y == 1 and 1 < coord_x < Human.length :
+            vectors = ['left', 'front', 'back']
+        elif coord_y == Human.width and 1 < coord_x < Human.length:
+            vectors = ['right', 'front', 'back']
+        elif coord_y == 1 and coord_x == 1:
+            vectors = ['left', 'front']
+        elif coord_y == 1 and coord_x == Human.length:
+            vectors = ['left', 'back']
+        elif coord_y == Human.width and coord_x == 1:
+            vectors = ['right', 'front']
+        elif coord_y == Human.width and coord_x == Human.length:
+            vectors = ['right', 'back']
+        # Уперся в правый край граница ось У ->
+        elif coord_x == Human.length and (coord_y != Human.length and coord_y != 1 ):
+            vectors = ['left', 'right', 'back']
+        
+    elif vector_face == 2:
+        if coord_x == 1 and 1 < coord_y < Human.length :
+            vectors = ['right', 'front', 'back']
+        elif coord_x == Human.width and 1 < coord_y < Human.length:
+            vectors = ['left', 'front', 'back']
+        elif coord_y == 1 and coord_x == 1:
+            vectors = ['right', 'front']
+        elif coord_y == 1 and coord_x == Human.length:
+            vectors = ['right', 'front']
+        elif coord_y == Human.width and coord_x == 1:
+            vectors = ['right', 'back'] 
+        elif coord_y == Human.width and coord_x == Human.length:
+            vectors = ['left', 'back']
+            # Уперся в верхний край граница ось У ->
+        elif coord_y == Human.length and (coord_x != Human.length and coord_x != 1 ):
+            vectors = ['left', 'right', 'back']
+        
+    elif vector_face == -1:
+        if coord_y == 1 and 1 < coord_x < Human.length :
+            vectors = ['right', 'front', 'back']
+        elif coord_y == Human.width and 1 < coord_x < Human.length:
+            vectors = ['left', 'front', 'back']
+        elif coord_y == 1 and coord_x == 1:  
+            vectors = ['right', 'back']
+        elif coord_y == 1 and coord_x == Human.length:
+            vectors = ['right', 'front']
+        elif coord_y == Human.width and coord_x == 1:
+            vectors = ['left', 'back']
+        elif coord_y == Human.width and coord_x == Human.length:
+            vectors = ['left', 'front']
+            # Уперся в левый край граница ось У ->
+        elif coord_x == 1 and (coord_y != Human.length and coord_y != 1 ):
+            vectors = ['left', 'right', 'back']
+        
+    elif vector_face == -2:
+        if coord_x == 1 and 1 < coord_y < Human.length :
+            vectors = ['left', 'front', 'back']
+        elif coord_x == Human.width and 1 < coord_y < Human.length:
+            vectors = ['right', 'front', 'back']
+        elif coord_y == 1 and coord_x == 1:
+            vectors = ['left', 'back']
+        elif coord_y == 1 and coord_x == Human.length:
+            vectors = ['right', 'back']
+        elif coord_y == Human.width and coord_x == 1:
+            vectors = ['left', 'front']
+        elif coord_y == Human.width and coord_x == Human.length:
+            vectors = ['right', 'back']
+            # Уперся в нижний край граница ось У ->
+        elif coord_y == 1 and (coord_x != Human.length and coord_x != 1 ):
+            vectors = ['left', 'right', 'back']
+    return vectors
+
+
+# Объявляется функция меняющие координаты персонажа
+def move_character(vertor_go, vector_face, coord_x, coord_y):
+    if hero.vector_face == 1:
+        if vertor_go == "8":
+            vector_face = 1
+            coord_x += 1
+        elif vertor_go == "2":
+            coord_x -= 1
+            vector_face = -1
+        elif vertor_go == "4":           
+            vector_face = 2
+            coord_y += 1
+        elif vertor_go == "6":
+            vector_face = -2
+            coord_y -= 1
        
-    # elif  hero.vector_face == 'y':
-    #     if vertor_go == "8":
-    #         hero.vector_face == 'y'
-    #         hero.coord_y += 1
-    #     elif vertor_go == "2":
-    #         hero.vector_face = "-y"
-    #         hero.coord_y -= 1
-    #     elif vertor_go == "4":           
-    #         hero.vector_face = "-x"
-    #         hero.coord_x -= 1
-    #     elif vertor_go == "6":
-    #         hero.vector_face = "x"
-    #         hero.coord_x += 1     
+    elif  hero.vector_face == 2:
+        if vertor_go == "8":
+            vector_face == 2
+            coord_y += 1
+        elif vertor_go == "2":
+            vector_face = -2
+            coord_y -= 1
+        elif vertor_go == "4":           
+            vector_face = -1
+            coord_x -= 1
+        elif vertor_go == "6":
+            vector_face = 1
+            coord_x += 1     
    
-    # elif hero.vector_face == '-x':
-    #     if vertor_go == "8":
-    #         hero.vector_face = "-x"
-    #         hero.coord_x -= 1
-    #     elif vertor_go == "2":
-    #         hero.coord_x += 1
-    #         hero.vector_face = "x"
-    #     elif vertor_go == "4":           
-    #         hero.vector_face = "-y"
-    #         hero.coord_y -= 1
-    #     elif vertor_go == "6":
-    #         hero.vector_face = "y"
-    #         hero.coord_y += 1
+    elif hero.vector_face == -1:
+        if vertor_go == "8":
+            vector_face = -1
+            coord_x -= 1
+        elif vertor_go == "2":
+            coord_x += 1
+            vector_face = 1
+        elif vertor_go == "4":           
+            vector_face = -2
+            coord_y -= 1
+        elif vertor_go == "6":
+            vector_face = 2
+            coord_y += 1
        
-    # elif  hero.vector_face == '-y':
-    #     if vertor_go == "8":
-    #         hero.coord_y -= 1
-    #     elif vertor_go == "2":
-    #         hero.vector_face = "y"
-    #         hero.coord_y += 1
-    #     elif vertor_go == "4":           
-    #         hero.vector_face = "x"
-    #         hero.coord_x += 1
-    #     elif vertor_go == "6":
-    #         hero.vector_face = "-x"
-    #         hero.coord_x -= 1     
-
-
+    elif  hero.vector_face == -2:
+        if vertor_go == "8":
+            coord_y -= 1
+        elif vertor_go == "2":
+            vector_face = 2
+            coord_y += 1
+        elif vertor_go == "4":           
+            vector_face = 1
+            coord_x += 1
+        elif vertor_go == "6":
+            vector_face = -1
+            coord_x -= 1     
+    return [coord_x, coord_y]
 
 
 
 
 for y in range(100):
-
-# Отображается поле и положение персонажа    
+# Создается поле     
     playing_field = [[0] * hero.length for i in range(hero.width)]
-   
-    
-    playing_field[hero.coord_y - 1][hero.coord_x - 1] = hero.vector_face
+# Направление стрелки ГГ    
+    if hero.vector_face % 2 and hero.vector_face > 0:
+        symbol = '>'
+    elif hero.vector_face % 2 and hero.vector_face < 0:
+         symbol = '<'
+    elif not hero.vector_face % 2 and hero.vector_face > 0:
+        symbol = '^'
+    else:
+        symbol = '|'
+# Размещение персонажа на карте    
+    playing_field[hero.coord_y - 1][hero.coord_x - 1] = symbol
+    playing_field[bot.coord_y - 1][bot.coord_x - 1] = '*'
     playing_field.reverse()
     for i in playing_field:
         print (i)    
+# Вызывается функция определения возможных передвижений персонажей    
+    vectors = possible_vector_go(bot.vector_face, bot.coord_y, bot.coord_x)
+    random_vector = random.randrange(2, 10, 2)
     
-    hero.possible_vector_go()
-    vectors = ""
-    for vector in hero.vectors:
+    vectors = possible_vector_go(hero.vector_face, hero.coord_y, hero.coord_x)
+    vectors1 = ""
+    for vector in vectors:
         if vector == 'back':
-            vectors += 'Назад - клавиша - 2 '
+            vectors1 += 'Назад - клавиша - 2 '
         elif vector == 'left':
-            vectors += 'Влево - клавиша - 4 '
+            vectors1 += 'Влево - клавиша - 4 '
         elif vector == 'right':
-            vectors += 'Вправо - клавиша - 6 '
+            vectors1 += 'Вправо - клавиша - 6 '
         elif vector == 'front':
-            vectors += 'Вперед - клавиша - 8 '
-   
+            vectors1+= 'Вперед - клавиша - 8 '
     while True:    
-        vector = input(print(f'Куда дальше? Возможные направления: {vectors}{hero.coord_x, hero.coord_y, hero.vector_face}'))    
-        int_arr = [i for i in vectors.split() if i.isnumeric() ]
+        vector = input(print(f'Куда дальше? Возможные направления: {vectors1}{hero.coord_x, hero.coord_y, hero.vector_face}'))    
+        int_arr = [i for i in vectors1.split() if i.isnumeric() ]
         if not vector in int_arr:
             print("Ой-бой!!! Тааак нельзя!!!! Сооберись")
         else:
             break
-            
-    move_hero(vector)
+    
+    
+    hero.coord_x, hero.coord_y = move_character(random_vector, hero.vector_face, hero.coord_x, hero.coord_y)        
+    bot.coord_x, bot.coord_y = move_character(vector, bot.vector_face, hero.coord_x, hero.coord_y)
+    
